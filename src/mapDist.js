ee44a9c382d21193ceb62367863646dddcb6c4a1
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2019-02-12 19:16:36 +0900

    コンフリクト解消

--- src/mapDist.js
+++ src/mapDist.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import MapView from 'react-native-maps';
-import PropTypes from 'prop-types';
+import MapView from 'react-native-maps-super-cluster';
+import { Marker } from 'react-native-maps';
 import { withNavigation } from 'react-navigation';
+import PropTypes from 'prop-types';
 import Modal from './modal';
 import global from './global';
 import touristSpotMarkerImg from '../assets/location.png';
+
+import 'date-utils';
 
 let data = [];
 
@@ -21,25 +24,63 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 
-  marker: {
+  mark_red: {
+    width: 15,
+    height: 15,
+    marginTop: 5,
+    marginRight: 2,
+    backgroundColor: 'red',
+    borderRadius: 10,
+  },
+  mark_blue: {
+    width: 15,
+    height: 15,
+    marginTop: 5,
+    marginRight: 2,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+  },
+  marker_red: {
     backgroundColor: 'red',
     padding: 5,
     borderRadius: 10,
   },
-  marker1: {
+  marker_blue: {
     backgroundColor: 'blue',
     padding: 5,
     borderRadius: 10,
   },
   text: {
     color: '#FFF',
+    fontSize: 12,
     fontWeight: 'bold',
+  },
+  text1: {
+    fontSize: 20,
+    marginTop: 2,
+    marginRight: 5,
+    fontWeight: 'bold',
+  },
+  counterText: {
+    fontSize: 12,
+    color: '#04B431',
+    fontWeight: 'bold',
+  },
+  clusterContainer: {
+    width: 24,
+    height: 24,
+    borderWidth: 2,
+    borderRadius: 12,
+    alignItems: 'center',
+    borderColor: '#04B431',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
   },
 });
 
 
 // 関数名や変数名の修正
-class MapDist extends React.Component {
+class Map extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -54,16 +95,61 @@ class MapDist extends React.Component {
     this.setState({ isOpen: true });
   }
 
+  detailScreen = () => {
+    const { navigation } = this.props;
+    navigation.navigate('DetailScreen', {
+      hotelName: data.HotelName,
+      hotelAddress: data.HotelAddress,
+      pictureURL: data.PictureURL,
+      planSampleRateFrom: data.PlanSampleRateFrom,
+      hotelUrl: data.HotelUrl,
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  convertPoints(data1) {
+    const results = {
+      type: 'MapCollection',
+      features: [],
+    };
+    // eslint-disable-next-line array-callback-return
+    data1.map((value) => {
+      // eslint-disable-next-line no-undef
+      array = {
+        value,
+        location: {
+          latitude: value.coordinates.latitude,
+          longitude: value.coordinates.longitude,
+        },
+      };
+      // eslint-disable-next-line no-undef
+      results.features.push(array);
+    });
+    return results.features;
+  }
+
+  renderMarker = pin => (
+    <Marker
+      key={pin.value.id}
+      coordinate={pin.location}
+      image={touristSpotMarkerImg}
+      title={pin.value.name}
+    />
+  )
+
+  renderCluster = (cluster, onPress) => (
+    <Marker coordinate={cluster.coordinate} onPress={onPress}>
+      <View style={styles.clusterContainer}>
+        <Text style={styles.counterText}>
+          {cluster.pointCount}
+        </Text>
+      </View>
+    </Marker>
+  )
+
   render() {
     const { isOpen } = this.state;
-    const { navigation } = this.props;
-    const { coordinates } = navigation.getParam('dist');
-    const region = {
-      latitude: coordinates.latitude,
-      longitude: coordinates.longitude,
-      latitudeDelta: 0.00922,
-      longitudeDelta: 0.00521,
-    };
+    const data1 = this.convertPoints(global.touristF);
     return (
       <View style={styles.container}>
         <Modal
@@ -74,8 +160,24 @@ class MapDist extends React.Component {
         />
         <MapView
           style={styles.mapview}
-          initialRegion={region}
+          data={data1}
+          animateClusters={false}
+          renderMarker={this.renderMarker}
+          renderCluster={this.renderCluster}
+          initialRegion={{
+            latitude: 36.5780818,
+            longitude: 136.6478206,
+            latitudeDelta: 0.00922,
+            longitudeDelta: 0.00521,
+          }}
+          zoomEnabled
         >
+          <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+            <View style={styles.mark_blue} />
+            <Text style={styles.text1}>空室</Text>
+            <View style={styles.mark_red} />
+            <Text style={styles.text1}>満室</Text>
+          </View>
           {
             // 宿泊施設にピンを配置
             global.lodgingF.map((lodgingFacilitie) => {
@@ -83,25 +185,8 @@ class MapDist extends React.Component {
               if (lodgingFacilitie.HotelID !== undefined) {
                 title = lodgingFacilitie.PlanSampleRateFrom;
               }
-              if (lodgingFacilitie.State === 'noVacancy') {
-                return (
-                  <MapView.Marker
-                    coordinate={{
-                      latitude: lodgingFacilitie.Y,
-                      longitude: lodgingFacilitie.X,
-                    }}
-                    onPress={() => this.gotoElementScreen(lodgingFacilitie)}
-                    key={lodgingFacilitie.HotelID}
-                  >
-                    <View style={styles.marker}>
-                      <Text style={styles.text}>
-                        {title}
-                      </Text>
-                    </View>
-                  </MapView.Marker>);
-              }
               return (
-                <MapView.Marker
+                <Marker
                   coordinate={{
                     latitude: lodgingFacilitie.Y,
                     longitude: lodgingFacilitie.X,
@@ -109,44 +194,24 @@ class MapDist extends React.Component {
                   onPress={() => this.gotoElementScreen(lodgingFacilitie)}
                   key={lodgingFacilitie.HotelID}
                 >
-                  <View style={styles.marker1}>
-                    <Text style={styles.text}>{title}</Text>
+                  <View style={lodgingFacilitie.State === 'vacancy' ? styles.marker_blue : styles.marker_red}>
+                    <Text style={styles.text}>
+                      {title}
+                    </Text>
                   </View>
-                </MapView.Marker>
-              );
+                </Marker>);
             })
           }
-          {
-            // 観光施設にピンを配置
-            global.touristF.map((touristFacilitie) => {
-              let title = '観光地名';
-              if (touristFacilitie.id !== undefined) {
-                title = touristFacilitie.name;
-              }
-              return (
-                <MapView.Marker
-                  coordinate={{
-                    latitude: touristFacilitie.coordinates.latitude,
-                    longitude: touristFacilitie.coordinates.longitude,
-                  }}
-                  title={title}
-                  key={touristFacilitie.id}
-                  image={touristSpotMarkerImg}
-                />
-              );
-            })
-          }
-
         </MapView>
       </View>
     );
   }
 }
 
-MapDist.propTypes = {
+Map.propTypes = {
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
   }).isRequired,
 };
 
-export default withNavigation(MapDist);
+export default withNavigation(Map);
