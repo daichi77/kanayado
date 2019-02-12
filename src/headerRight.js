b5a8cdaf8128e5fd6e19e1bc1ec5c8a17d30c2a0
takahito1220 <b1606966@planet.kanazawa-it.ac.jp>
2019-02-12 14:29:17 +0900

    データピッカーとヘッダーのボタンの結合

--- src/headerRight.js
+++ src/headerRight.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import CalendarIcon from 'react-native-vector-icons/EvilIcons';
+import { TouchableOpacity, View } from 'react-native';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
-const HeaderRight = () => (
-  <TouchableOpacity>
-    <CalendarIcon
-      name="calendar"
-      size={35}
-      style={{ marginRight: 10 }}
-    />
-  </TouchableOpacity>
-);
+class TimePicker extends React.Component {
+  state = {
+    isDateTimePickerVisible: false,
+  };
 
-export default withNavigation(HeaderRight);
+  showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  handleDatePicked = (date) => {
+    console.log('A date has been picked: ', date);
+    this.hideDateTimePicker();
+  };
+
+  render() {
+    const { isDateTimePickerVisible } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity onPress={this.showDateTimePicker}>
+          <CalendarIcon
+            name="calendar"
+            size={35}
+            style={{ marginRight: 10 }}
+          />
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={isDateTimePickerVisible}
+          onConfirm={this.handleDatePicked}
+          onCancel={this.hideDateTimePicker}
+          titleIOS="日付を選択してください"
+          cancelTextIOS="キャンセル"
+          confirmTextIOS="決定"
+        />
+      </View>
+    );
+  }
+}
+
+export default withNavigation(TimePicker);
