719ec6e8c0602d795727ef8e61185442fedbf2ea
tsurusekazuki <b1714935@planet.kanazawa-it.ac.jp>
2020-02-16 21:19:59 +0900

    rename components -> Components and Add actionTypes

---
+++ src/Constants/actionTypes.js
@@ -0,0 +1,17 @@
+export const GET_POSTS_REQUEST = 'GET_POSTS_REQUEST';
+
+export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
+
+export const GET_POSTS_REQ = 'GET_POSTS_REQ';
+
+export const GET_POSTS_SUC = 'GET_POSTS_SUC';
+
+export const GET_POSTS_FAI = 'GET_POSTS_FAI';
+
+export const SEND_FILTER_TEXT = 'SEND_FILTER_TEXT';
+
+export const MODAL_IS_OPEN = 'MODAL_IS_OPEN';
+
+export const TOGGLE_MODAL_IS_OPEN = 'TOGGLE_MODAL_IS_OPEN';
+
+export const SAVE_HOTEL_DATA = 'SAVE_HOTEL_DATA';
